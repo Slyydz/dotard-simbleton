029dae7d6054bdd3beb8641f2eb5429051f6edd2
Brady Williams <williams.cole29@yahoo.com>
2021-08-29 17:53:32 -0500

    Finished Dotard And Simbleton

--- scripts/BusinessList.js
+++ scripts/BusinessList.js
@@ -6,6 +6,7 @@ import { purchaseArray } from "./BusinessData.js"
 
 const contentTarget = document.querySelector(".business");
 const contentTargetAgent = document.querySelector(".agents");
+const businessArray = useBusinesses();
 
 export const BusinessList = () => {
     const businessArray = useBusinesses();
@@ -39,3 +40,44 @@ export const AgentList = () => {
     contentTarget.innerHTML += Agent(count);
 });
 }
+
+// Place an article element in your HTML with the class below
+const companySearchResultArticle = document.querySelector(".foundCompanies")
+
+document
+    .querySelector("#companySearch")
+    .addEventListener("keypress", keyPressEvent => {
+        const arrayUse = useBusinesses();
+        if (keyPressEvent.charCode === 13) {
+            /*
+                When user presses enter, find the matching business.
+                You can use the `.includes()` method strings to
+                see if a smaller string is part of a larger string.
+
+                Example:
+                    business.companyName.includes(keyPressEvent.target.value)
+            */
+
+                
+
+            const foundBusiness = arrayUse.find(countVar => (countVar.purchasingAgent.nameFirst.includes(keyPressEvent.target.value)))
+
+            companySearchResultArticle.innerHTML = `
+                <h2>
+                ${foundBusiness.companyName}
+                </h2>
+                <h4>
+                ${foundBusiness.purchasingAgent.nameFirst} ${foundBusiness.purchasingAgent.nameLast}
+                </h4>
+                <section>
+                ${foundBusiness.addressFullStreet}
+
+                </section>
+                <section>
+                ${foundBusiness.addressCity},
+                ${foundBusiness.addressStateCode}
+                ${foundBusiness.addressZipCode}
+                </section>
+            `;
+        }
+    });
