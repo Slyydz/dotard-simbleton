c0b318d5a1c6d660c124eb62ccfb367a4ec5bf6d
Brady Williams <williams.cole29@yahoo.com>
2021-08-29 17:18:41 -0500

    Finished map section extra

--- scripts/Business.js
+++ scripts/Business.js
@@ -1,8 +1,8 @@
 export const Business = (businessObject) => {
     return `
         <section class="supply">
-            <h2 class="company__name">${businessObject.companyName}</h2>
-            <div class="company__address">
+            <h2 class="company-name">${businessObject.companyName}</h2>
+            <div class="company-address">
                 ${businessObject.addressFullStreet}, ${businessObject.addressStateCode}
             </div>
         </section>
@@ -12,7 +12,9 @@ export const Business = (businessObject) => {
 export const Agent = (agentObject) => {
     return `
         <section class="purchase-agent">
-            <h2 class="agent-name">${agentObject.nameFirst} ${agentObject.nameLast}</h2>
+            <h2 class="agent-name">${agentObject.name.nameFirst} ${agentObject.name.nameLast}</h2>
+            <h4 class ="company-name">${agentObject.company}</h4>
+            <p class="phone-number">${agentObject.phone}</p>
         </section>
     `
 }
